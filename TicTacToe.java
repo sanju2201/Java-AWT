50636e25cdf5529fb246d45b9909557d5d8c6d3a
SanjuManna <sanjumanna2201@gmail.com>
2022-10-31 10:48:09 +0530

    commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -63,6 +63,92 @@ public class TicTacToe extends Frame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+        // b1
+        if (e.getSource() == b1) {
+            if (t % 2 == 1)
+                b1.setLabel("x");
+            else
+                b1.setLabel("o");
+            t++;
+        }
+
+        // b2
+        if (e.getSource() == b2) {
+            if (t % 2 == 1)
+                b2.setLabel("x");
+            else
+                b2.setLabel("o");
+            t++;
+        }
+        // b3
+        if (e.getSource() == b3) {
+            if (t % 2 == 1)
+                b3.setLabel("x");
+            else
+                b3.setLabel("o");
+            t++;
+        }
+        // b4
+        if (e.getSource() == b4) {
+            if (t % 2 == 1)
+                b4.setLabel("x");
+            else
+                b4.setLabel("o");
+            t++;
+        }
+        // b5
+        if (e.getSource() == b5) {
+            if (t % 2 == 1)
+                b5.setLabel("x");
+            else
+                b5.setLabel("o");
+            t++;
+        }
+        // b6
+        if (e.getSource() == b6) {
+            if (t % 2 == 1)
+                b6.setLabel("x");
+            else
+                b6.setLabel("o");
+            t++;
+        }
+
+        // b7
+        if (e.getSource() == b7) {
+            if (t % 2 == 1)
+                b7.setLabel("x");
+            else
+                b7.setLabel("o");
+            t++;
+        }
+
+        // b8
+        if (e.getSource() == b8) {
+            if (t % 2 == 1)
+                b8.setLabel("x");
+            else
+                b8.setLabel("o");
+            t++;
+        }
+
+        // b9
+        if (e.getSource() == b9) {
+            if (t % 2 == 1)
+                b9.setLabel("x");
+            else
+                b9.setLabel("o");
+            t++;
+        }
+
+        String a = b1.getLabel();
+        String b = b2.getLabel();
+        String c = b3.getLabel();
+        String d = b1.getLabel();
+        String e1 = b1.getLabel();
+        String f = b1.getLabel();
+        String g = b1.getLabel();
+        String h = b1.getLabel();
+        String i = b1.getLabel();
 
     }
 
